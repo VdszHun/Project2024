9b3814d054964a3de7d09feb3128a2fa29cbddac
VdszHun <vdszdavid@gmail.com>
2024-01-28 16:32:24 +0100

    10th commit

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('CSS/style.css')}}">
     
 </head>
 <body class="eotvos_bg">
