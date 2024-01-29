4e45634b9b15d7fa38dd463548a5363c3a9788a0
szucserikdaniel <szucserikdaniel@EOTVOS2.LOCAL>
2024-01-29 16:14:28 +0100

    elrendezés, és ötletek

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,10 +15,10 @@
 
             <div class="row">
 
-                <div class="col-sm-12 col-md-8 col-lg-3 m-5">
+                <div class="col-smb-12 col-md-12 col-lg-5 mb-1">
 
-                    <a href="{{route('fusterzekelo1')}}">
-                    <div class="card eotvos_leftmenu" style="width: 18rem;">
+                    <a class="fooldal_link_no_deco" href="{{route('fusterzekelo1')}}">
+                    <div class="card eotvos_leftmenu" style="width: 12rem;">
                         <img src="{{asset('IMGS/terem_alks_048.jpg')}}" class="card-img-top" title="terem: 048" alt="terem: 048">
                         <div class="card-body">
                           <p class="card-text"><h4>TEREM: 048</h4><br/>Fiú mosdó</p>
@@ -28,10 +28,10 @@
 
                 </div>
             
-                <div class="col-sm-12 col-md-8 col-lg-3 m-5">
+                <div class="col-smb-12 col-md-12 col-lg-5 mb-1">
                  
-                    <a href="{{route('fusterzekelo2')}}">
-                    <div class="card eotvos_leftmenu" style="width: 18rem;">
+                    <a class="fooldal_link_no_deco" href="{{route('fusterzekelo2')}}">
+                    <div class="card eotvos_leftmenu" style="width: 12rem;">
                         <img src="imgs/terem_alks_048.jpg" class="card-img-top" title="terem: 047" alt="terem: 047">
                         <div class="card-body">
                           <p class="card-text"><h4>TEREM: 047</h4><br/>Női mosdó</p>
@@ -51,52 +51,52 @@
 
             <div class="row">
 
-                <div class="col-sm-12 col-md-8 col-lg-3 m-5">
+                <div class="col-smb-12 col-md-5 col-lg-3 mb-1">
 
-                    <a href="{{route('fusterzekelo1')}}">
-                    <div class="card eotvos_leftmenu" style="width: 18rem;">
-                        <img src="{{asset('IMGS/terem_alks_048.jpg')}}" class="card-img-top" title="terem: 048" alt="terem: 148">
+                    <a class="fooldal_link_no_deco" href="{{route('fusterzekelo1')}}">
+                    <div class="card eotvos_leftmenu" style="width: 12rem;">
+                        <img src="{{asset('IMGS/terem_alks_048.jpg')}}" class="card-img-top" title="terem: 148" alt="terem: 148">
                         <div class="card-body">
-                          <p class="card-text"><h4>TEREM: 048</h4><br/>Fiú mosdó</p>
+                          <p class="card-text"><h4>TEREM: 148</h4><br/>Fiú mosdó</p>
                         </div>
                       </div>
                     </a>
 
                 </div>
             
-                <div class="col-sm-12 col-md-8 col-lg-3 m-5">
+                <div class="col-smb-12 col-md-5 col-lg-3 mb-1">
                  
-                    <a href="{{route('fusterzekelo2')}}">
-                    <div class="card eotvos_leftmenu" style="width: 18rem;">
-                        <img src="imgs/terem_alks_048.jpg" class="card-img-top" title="terem: 047" alt="terem: 147">
+                    <a class="fooldal_link_no_deco" href="{{route('fusterzekelo2')}}">
+                    <div class="card eotvos_leftmenu" style="width: 12rem;">
+                        <img src="imgs/terem_alks_048.jpg" class="card-img-top" title="terem: 147" alt="terem: 147">
                         <div class="card-body">
-                          <p class="card-text"><h4>TEREM: 047</h4><br/>Női mosdó</p>
+                          <p class="card-text"><h4>TEREM: 147</h4><br/>Női mosdó</p>
                         </div>
                       </div>
                     </a>
 
                 </div>
             
-                <div class="col-sm-12 col-md-8 col-lg-3 m-5">
+                <div class="col-smb-12 col-md-4 col-lg-3 mb-1">
 
-                    <a href="{{route('fusterzekelo1')}}">
-                    <div class="card eotvos_leftmenu" style="width: 18rem;">
-                        <img src="{{asset('IMGS/terem_alks_048.jpg')}}" class="card-img-top" title="terem: 048" alt="terem: 108">
+                    <a class="fooldal_link_no_deco" href="{{route('fusterzekelo1')}}">
+                    <div class="card eotvos_leftmenu" style="width: 12rem;">
+                        <img src="{{asset('IMGS/terem_alks_048.jpg')}}" class="card-img-top" title="terem: 108" alt="terem: 108">
                         <div class="card-body">
-                          <p class="card-text"><h4>TEREM: 048</h4><br/>Fiú mosdó</p>
+                          <p class="card-text"><h4>TEREM: 108</h4><br/>Fiú mosdó</p>
                         </div>
                       </div>
                     </a>
 
                 </div>
             
-                <div class="col-sm-12 col-md-8 col-lg-6 m-5">
+                <div class="col-smb-12 col-md-4 col-lg-3 mb-1">
                  
-                    <a href="{{route('fusterzekelo2')}}">
-                    <div class="card eotvos_leftmenu" style="width: 18rem;">
-                        <img src="imgs/terem_alks_048.jpg" class="card-img-top" title="terem: 047" alt="terem: 107">
+                    <a class="fooldal_link_no_deco" href="{{route('fusterzekelo2')}}">
+                    <div class="card eotvos_leftmenu" style="width: 12rem;">
+                        <img src="imgs/terem_alks_048.jpg" class="card-img-top" title="terem: 107" alt="terem: 107">
                         <div class="card-body">
-                          <p class="card-text"><h4>TEREM: 047</h4><br/>Női mosdó</p>
+                          <p class="card-text"><h4>TEREM: 107</h4><br/>Női mosdó</p>
                         </div>
                       </div>
                     </a>
@@ -112,9 +112,70 @@
         <td>
             <h3>Első elemet</h3>
 
+            <div class="row">
+
+              <div class="col-smb-12 col-md-12 col-lg-5 mb-1">
+
+                  <a class="fooldal_link_no_deco" href="{{route('fusterzekelo1')}}">
+                  <div class="card eotvos_leftmenu" style="width: 12rem;">
+                      <img src="{{asset('IMGS/terem_alks_048.jpg')}}" class="card-img-top" title="terem: 248" alt="terem: 248">
+                      <div class="card-body">
+                        <p class="card-text"><h4>TEREM: 248</h4><br/>Fiú mosdó</p>
+                      </div>
+                    </div>
+                  </a>
+
+              </div>
+          
+              <div class="col-smb-12 col-md-12 col-lg-5 mb-1">
+               
+                  <a class="fooldal_link_no_deco" href="{{route('fusterzekelo2')}}">
+                  <div class="card eotvos_leftmenu" style="width: 12rem;">
+                      <img src="imgs/terem_alks_048.jpg" class="card-img-top" title="terem: 247" alt="terem: 247">
+                      <div class="card-body">
+                        <p class="card-text"><h4>TEREM: 247</h4><br/>Női mosdó</p>
+                      </div>
+                    </div>
+                  </a>
+
+                </div>
+          
+          </div>
+
         </td>
         <td>
             <h3>Második elemet</h3>
+
+            <div class="row">
+
+              <div class="col-smb-12 col-md-8 col-lg-3 mb-1">
+
+                  <a class="fooldal_link_no_deco" href="{{route('fusterzekelo1')}}">
+                  <div class="card eotvos_leftmenu" style="width: 12rem;">
+                      <img src="{{asset('IMGS/terem_alks_048.jpg')}}" class="card-img-top" title="terem: 348" alt="terem: 348">
+                      <div class="card-body">
+                        <p class="card-text"><h4>TEREM: 348</h4><br/>Fiú mosdó</p>
+                      </div>
+                    </div>
+                  </a>
+
+              </div>
+          
+              <div class="col-smb-12 col-md-8 col-lg-3 mb-1">
+               
+                  <a class="fooldal_link_no_deco" href="{{route('fusterzekelo2')}}">
+                  <div class="card eotvos_leftmenu" style="width: 12rem;">
+                      <img src="imgs/terem_alks_048.jpg" class="card-img-top" title="terem: 347" alt="terem: 347">
+                      <div class="card-body">
+                        <p class="card-text"><h4>TEREM: 347</h4><br/>Női mosdó</p>
+                      </div>
+                    </div>
+                  </a>
+
+                </div>
+          
+          </div>
+
         </td>
     </tr>
 </table>
